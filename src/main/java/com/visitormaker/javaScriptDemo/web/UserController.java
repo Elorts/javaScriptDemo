8b54e6843da1844fc8bfff2b82b0904479d9168e
deividas strole <deiwis@yahoo.com>
2024-07-10 20:16:36 -0700

    ver 3

---
+++ src/main/java/com/visitormaker/javaScriptDemo/web/UserController.java
@@ -0,0 +1,27 @@
+package com.visitormaker.javaScriptDemo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/exists")
+    public Boolean getExists(String username) {
+        return true;
+    }
+
+    @GetMapping("/validateUsername")
+    public Boolean getValidUsername(String name) {
+        return true;
+    }
+
+    @GetMapping("/validatePassword")
+    public Boolean getValidPassword(String password) {
+        return true;
+    }
+
+
+}
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,19 +1,28 @@
 var submitBtn = document.querySelector("#submitBtn")
+var username = document.querySelector("#username")
 var users = []
 
-            submitBtn.addEventListener('click', () => {
-                var username = document.querySelector("#username")
-                var password = document.querySelector("#password")
+submitBtn.addEventListener('click', () => {
+    var username = document.querySelector("#username")
+    var password = document.querySelector("#password")
 
-                if (username.value == '' || password.value == '') {
-                    alert("Please enter username and password")
-                } else {
-                    alert("All good!")
-                    var user = {
-                        "username" : username.value,
-                        "password" : password.value
-                    }
+    if (username.value == '' || password.value == '') {
+        alert("Please enter username and password")
+    } else {
+        alert("All good!")
+        var user = {
+            "username" : username.value,
+            "password" : password.value
+        }
 
-                    users.push(user);
-                }
-            })
+        users.push(user);
+    }
+})
+
+username.addEventListener("blur", () => {
+   // console.log("We have a blur event");
+   fetch("http://localhost:8080/users/exists")
+    .then((response) => {
+        console.log(response)
+    })
+})
